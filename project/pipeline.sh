be769d5be30ee7f3854cd250a92b3a916c0f67cc
Abdul Basit <abdulbasit@Macbook-von-Abdul.fritz.box>
2024-11-10 21:19:02 +0100

    Add comments

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Some systems use the pip command and some use pip3. This handles that
 if command -v pip3 &> /dev/null
 then
     PIP_COMMAND="pip3"
@@ -11,7 +12,9 @@ else
     exit 1
 fi
 
-# Use the detected pip command to install packages
+
 $PIP_COMMAND install pandas sqlalchemy requests opendatasets fuzzywuzzy
 
+# This is to make sure that no matter from where this script is called, this will call the pipeline.py 
+# from its own directory
 python3 "$(dirname "$0")/pipeline.py"
